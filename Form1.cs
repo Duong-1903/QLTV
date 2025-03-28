b1fee0126be1b1d83885fadd2b9f5404c1729942
Duong-1903 <duongtd1903@gmail.com>
2025-03-28 15:16:35 +0700

    làm bài đi
    Add button1 with click event to Form1
    
    Added a new button (`button1`) to the form in `Form1.Designer.cs`, setting its properties and adding it to the controls. Implemented the `button1_Click_1` event handler in `Form1.cs` to display a message box with "Làm bài đi" when clicked. The previous `button1_Click` handler remains empty for potential future use.

--- Form1.cs
+++ Form1.cs
@@ -1,10 +1,20 @@
-namespace QLTV
+﻿namespace QLTV
 {
     public partial class Form1 : Form
     {
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            MessageBox.Show("Làm bài đi");
         }
     }
 }
