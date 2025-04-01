5579d2ba777d10201410891aad732da256dbb4e0
Duong-1903 <duongtd1903@gmail.com>
2025-04-01 18:55:35 +0700

    Đăng nhập

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,32 +28,427 @@
         /// </summary>
         private void InitializeComponent()
         {
+            pnl_dangnhap = new Panel();
+            txt_matkhau = new TextBox();
+            lbl_quenmatkhau = new Label();
+            lbl_matkhau = new Label();
+            lbl_taotaikhoan = new Label();
+            lbl_taikhoan = new Label();
+            txt_taikhoan = new TextBox();
             button1 = new Button();
+            pnl_taotaikhoan = new Panel();
+            txt_xnmatkhau = new TextBox();
+            textBox5 = new TextBox();
+            txt_taomatkhau = new TextBox();
+            textBox3 = new TextBox();
+            label7 = new Label();
+            label3 = new Label();
+            label5 = new Label();
+            btn_taomatkhau = new Button();
+            txt_taotaikhoan = new TextBox();
+            label6 = new Label();
+            pnl_quenmatkhau = new Panel();
+            txt_xnmatkhaumoi = new TextBox();
+            textBox2 = new TextBox();
+            txt_matkhaumoi = new TextBox();
+            textBox6 = new TextBox();
+            label1 = new Label();
+            label2 = new Label();
+            label4 = new Label();
+            btn_capnhatmatkhau = new Button();
+            txt_timtaikhoan = new TextBox();
+            label8 = new Label();
+            pnl_dangnhap.SuspendLayout();
+            pnl_taotaikhoan.SuspendLayout();
+            pnl_quenmatkhau.SuspendLayout();
             SuspendLayout();
+            // 
+            // pnl_dangnhap
+            // 
+            pnl_dangnhap.Controls.Add(txt_matkhau);
+            pnl_dangnhap.Controls.Add(lbl_quenmatkhau);
+            pnl_dangnhap.Controls.Add(lbl_matkhau);
+            pnl_dangnhap.Controls.Add(lbl_taotaikhoan);
+            pnl_dangnhap.Controls.Add(lbl_taikhoan);
+            pnl_dangnhap.Controls.Add(txt_taikhoan);
+            pnl_dangnhap.Controls.Add(button1);
+            pnl_dangnhap.Font = new Font("Times New Roman", 10.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            pnl_dangnhap.Location = new Point(183, 41);
+            pnl_dangnhap.Name = "pnl_dangnhap";
+            pnl_dangnhap.Size = new Size(334, 219);
+            pnl_dangnhap.TabIndex = 0;
+            // 
+            // txt_matkhau
+            // 
+            txt_matkhau.Location = new Point(157, 54);
+            txt_matkhau.Name = "txt_matkhau";
+            txt_matkhau.Size = new Size(174, 27);
+            txt_matkhau.TabIndex = 2;
+            txt_matkhau.KeyPress += txt_matkhau_KeyPress;
+            // 
+            // lbl_quenmatkhau
+            // 
+            lbl_quenmatkhau.AutoSize = true;
+            lbl_quenmatkhau.ForeColor = Color.DeepSkyBlue;
+            lbl_quenmatkhau.Location = new Point(104, 119);
+            lbl_quenmatkhau.Name = "lbl_quenmatkhau";
+            lbl_quenmatkhau.Size = new Size(128, 19);
+            lbl_quenmatkhau.TabIndex = 1;
+            lbl_quenmatkhau.Text = "Quên mật khẩu?";
+            lbl_quenmatkhau.Click += lbl_quenmatkhau_Click;
+            // 
+            // lbl_matkhau
+            // 
+            lbl_matkhau.AutoSize = true;
+            lbl_matkhau.Location = new Point(3, 57);
+            lbl_matkhau.Name = "lbl_matkhau";
+            lbl_matkhau.Size = new Size(84, 19);
+            lbl_matkhau.TabIndex = 1;
+            lbl_matkhau.Text = "Mật khẩu:";
+            // 
+            // lbl_taotaikhoan
+            // 
+            lbl_taotaikhoan.AutoSize = true;
+            lbl_taotaikhoan.ForeColor = Color.DeepSkyBlue;
+            lbl_taotaikhoan.Location = new Point(114, 88);
+            lbl_taotaikhoan.Name = "lbl_taotaikhoan";
+            lbl_taotaikhoan.Size = new Size(108, 19);
+            lbl_taotaikhoan.TabIndex = 1;
+            lbl_taotaikhoan.Text = "Tạo tài khoản";
+            lbl_taotaikhoan.Click += lbl_taotaikhoan_Click;
+            // 
+            // lbl_taikhoan
+            // 
+            lbl_taikhoan.AutoSize = true;
+            lbl_taikhoan.Location = new Point(3, 28);
+            lbl_taikhoan.Name = "lbl_taikhoan";
+            lbl_taikhoan.Size = new Size(87, 19);
+            lbl_taikhoan.TabIndex = 1;
+            lbl_taikhoan.Text = "Tài khoản:";
+            // 
+            // txt_taikhoan
+            // 
+            txt_taikhoan.Location = new Point(157, 26);
+            txt_taikhoan.Name = "txt_taikhoan";
+            txt_taikhoan.Size = new Size(174, 27);
+            txt_taikhoan.TabIndex = 2;
+            txt_taikhoan.KeyPress += txt_taikhoan_KeyPress;
             // 
             // button1
             // 
-            button1.Location = new Point(213, 52);
+            button1.Location = new Point(112, 150);
             button1.Name = "button1";
-            button1.Size = new Size(94, 29);
+            button1.Size = new Size(118, 25);
             button1.TabIndex = 0;
-            button1.Text = "Nhấn nó đi";
+            button1.Text = "Đăng nhập";
             button1.UseVisualStyleBackColor = true;
-            button1.Click += button1_Click_1;
+            // 
+            // pnl_taotaikhoan
+            // 
+            pnl_taotaikhoan.Controls.Add(txt_xnmatkhau);
+            pnl_taotaikhoan.Controls.Add(textBox5);
+            pnl_taotaikhoan.Controls.Add(txt_taomatkhau);
+            pnl_taotaikhoan.Controls.Add(textBox3);
+            pnl_taotaikhoan.Controls.Add(label7);
+            pnl_taotaikhoan.Controls.Add(label3);
+            pnl_taotaikhoan.Controls.Add(label5);
+            pnl_taotaikhoan.Controls.Add(btn_taomatkhau);
+            pnl_taotaikhoan.Controls.Add(txt_taotaikhoan);
+            pnl_taotaikhoan.Controls.Add(label6);
+            pnl_taotaikhoan.Font = new Font("Times New Roman", 10.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            pnl_taotaikhoan.Location = new Point(342, 266);
+            pnl_taotaikhoan.Name = "pnl_taotaikhoan";
+            pnl_taotaikhoan.Size = new Size(334, 219);
+            pnl_taotaikhoan.TabIndex = 0;
+            pnl_taotaikhoan.Visible = false;
+            // 
+            // txt_xnmatkhau
+            // 
+            txt_xnmatkhau.Location = new Point(157, 82);
+            txt_xnmatkhau.Name = "txt_xnmatkhau";
+            txt_xnmatkhau.Size = new Size(174, 27);
+            txt_xnmatkhau.TabIndex = 2;
+            txt_xnmatkhau.KeyPress += txt_xnmatkhau_KeyPress;
+            // 
+            // textBox5
+            // 
+            textBox5.Location = new Point(157, 82);
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(125, 27);
+            textBox5.TabIndex = 2;
+            // 
+            // txt_taomatkhau
+            // 
+            txt_taomatkhau.Location = new Point(157, 54);
+            txt_taomatkhau.Name = "txt_taomatkhau";
+            txt_taomatkhau.Size = new Size(174, 27);
+            txt_taomatkhau.TabIndex = 2;
+            txt_taomatkhau.KeyPress += txt_taomatkhau_KeyPress;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(157, 54);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(125, 27);
+            textBox3.TabIndex = 2;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(3, 84);
+            label7.Name = "label7";
+            label7.Size = new Size(153, 19);
+            label7.TabIndex = 1;
+            label7.Text = "Xác nhận mật khẩu:";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(3, 56);
+            label3.Name = "label3";
+            label3.Size = new Size(84, 19);
+            label3.TabIndex = 1;
+            label3.Text = "Mật khẩu:";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(3, 28);
+            label5.Name = "label5";
+            label5.Size = new Size(87, 19);
+            label5.TabIndex = 1;
+            label5.Text = "Tài khoản:";
+            // 
+            // btn_taomatkhau
+            // 
+            btn_taomatkhau.Location = new Point(112, 150);
+            btn_taomatkhau.Name = "btn_taomatkhau";
+            btn_taomatkhau.Size = new Size(118, 25);
+            btn_taomatkhau.TabIndex = 0;
+            btn_taomatkhau.Text = "Tạo tài khoản";
+            btn_taomatkhau.UseVisualStyleBackColor = true;
+            btn_taomatkhau.Click += btn_taomatkhau_Click;
+            // 
+            // txt_taotaikhoan
+            // 
+            txt_taotaikhoan.Location = new Point(157, 26);
+            txt_taotaikhoan.Name = "txt_taotaikhoan";
+            txt_taotaikhoan.Size = new Size(174, 27);
+            txt_taotaikhoan.TabIndex = 2;
+            txt_taotaikhoan.KeyPress += txt_taotaikhoan_KeyPress;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(3, 28);
+            label6.Name = "label6";
+            label6.Size = new Size(55, 19);
+            label6.TabIndex = 1;
+            label6.Text = "label1";
+            // 
+            // pnl_quenmatkhau
+            // 
+            pnl_quenmatkhau.Controls.Add(txt_xnmatkhaumoi);
+            pnl_quenmatkhau.Controls.Add(textBox2);
+            pnl_quenmatkhau.Controls.Add(txt_matkhaumoi);
+            pnl_quenmatkhau.Controls.Add(textBox6);
+            pnl_quenmatkhau.Controls.Add(label1);
+            pnl_quenmatkhau.Controls.Add(label2);
+            pnl_quenmatkhau.Controls.Add(label4);
+            pnl_quenmatkhau.Controls.Add(btn_capnhatmatkhau);
+            pnl_quenmatkhau.Controls.Add(txt_timtaikhoan);
+            pnl_quenmatkhau.Controls.Add(label8);
+            pnl_quenmatkhau.Font = new Font("Times New Roman", 10.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            pnl_quenmatkhau.Location = new Point(2, 266);
+            pnl_quenmatkhau.Name = "pnl_quenmatkhau";
+            pnl_quenmatkhau.Size = new Size(334, 219);
+            pnl_quenmatkhau.TabIndex = 0;
+            pnl_quenmatkhau.Visible = false;
+            // 
+            // txt_xnmatkhaumoi
+            // 
+            txt_xnmatkhaumoi.Location = new Point(157, 82);
+            txt_xnmatkhaumoi.Name = "txt_xnmatkhaumoi";
+            txt_xnmatkhaumoi.Size = new Size(174, 27);
+            txt_xnmatkhaumoi.TabIndex = 2;
+            txt_xnmatkhaumoi.KeyPress += txt_xnmatkhaumoi_KeyPress;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(157, 82);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(125, 27);
+            textBox2.TabIndex = 2;
+            // 
+            // txt_matkhaumoi
+            // 
+            txt_matkhaumoi.Location = new Point(157, 54);
+            txt_matkhaumoi.Name = "txt_matkhaumoi";
+            txt_matkhaumoi.Size = new Size(174, 27);
+            txt_matkhaumoi.TabIndex = 2;
+            txt_matkhaumoi.KeyPress += txt_matkhaumoi_KeyPress;
+            // 
+            // textBox6
+            // 
+            textBox6.Location = new Point(157, 54);
+            textBox6.Name = "textBox6";
+            textBox6.Size = new Size(125, 27);
+            textBox6.TabIndex = 2;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(3, 84);
+            label1.Name = "label1";
+            label1.Size = new Size(153, 19);
+            label1.TabIndex = 1;
+            label1.Text = "Xác nhận mật khẩu:";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(3, 56);
+            label2.Name = "label2";
+            label2.Size = new Size(117, 19);
+            label2.TabIndex = 1;
+            label2.Text = "Mật khẩu mới:";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(3, 28);
+            label4.Name = "label4";
+            label4.Size = new Size(87, 19);
+            label4.TabIndex = 1;
+            label4.Text = "Tài khoản:";
+            // 
+            // btn_capnhatmatkhau
+            // 
+            btn_capnhatmatkhau.Location = new Point(112, 150);
+            btn_capnhatmatkhau.Name = "btn_capnhatmatkhau";
+            btn_capnhatmatkhau.Size = new Size(118, 25);
+            btn_capnhatmatkhau.TabIndex = 0;
+            btn_capnhatmatkhau.Text = "Cập nhật ";
+            btn_capnhatmatkhau.UseVisualStyleBackColor = true;
+            btn_capnhatmatkhau.Click += btn_capnhatmatkhau_Click;
+            // 
+            // txt_timtaikhoan
+            // 
+            txt_timtaikhoan.Location = new Point(157, 26);
+            txt_timtaikhoan.Name = "txt_timtaikhoan";
+            txt_timtaikhoan.Size = new Size(174, 27);
+            txt_timtaikhoan.TabIndex = 2;
+            txt_timtaikhoan.KeyPress += txt_timtaikhoan_KeyPress;
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Location = new Point(3, 28);
+            label8.Name = "label8";
+            label8.Size = new Size(55, 19);
+            label8.TabIndex = 1;
+            label8.Text = "label1";
             // 
             // Form1
             // 
-            AutoScaleDimensions = new SizeF(8F, 20F);
-            AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
-            Controls.Add(button1);
+            AccessibleRole = AccessibleRole.ScrollBar;
+            AutoScaleMode = AutoScaleMode.Inherit;
+            ClientSize = new Size(678, 524);
+            Controls.Add(pnl_dangnhap);
+            Controls.Add(pnl_quenmatkhau);
+            Controls.Add(pnl_taotaikhoan);
+            Font = new Font("Times New Roman", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
             Name = "Form1";
-            Text = "Thư viện";
+            SizeGripStyle = SizeGripStyle.Hide;
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Đăng nhập";
+            TopMost = true;
+            pnl_dangnhap.ResumeLayout(false);
+            pnl_dangnhap.PerformLayout();
+            pnl_taotaikhoan.ResumeLayout(false);
+            pnl_taotaikhoan.PerformLayout();
+            pnl_quenmatkhau.ResumeLayout(false);
+            pnl_quenmatkhau.PerformLayout();
             ResumeLayout(false);
+
+            // Set initial visibility
+            pnl_dangnhap.Visible = true;
+            pnl_taotaikhoan.Visible = false;
+            pnl_quenmatkhau.Visible = false;
+
+            // Add show/hide password buttons
+            Button btn_showHidePassword1 = new Button();
+            btn_showHidePassword1.Text = "👁";
+            btn_showHidePassword1.Size = new Size(30, 27);
+            btn_showHidePassword1.Location = new Point(txt_matkhau.Width - btn_showHidePassword1.Width, 0);
+//            btn_showHidePassword1.MouseDown += (s, e) => txt_matkhau.UseSystemPasswordChar = false;
+ //           btn_showHidePassword1.MouseUp += (s, e) => txt_matkhau.UseSystemPasswordChar = true;
+            txt_matkhau.Controls.Add(btn_showHidePassword1);
+
+            Button btn_showHidePassword2 = new Button();
+            btn_showHidePassword2.Text = "👁";
+            btn_showHidePassword2.Size = new Size(30, 27);
+            btn_showHidePassword2.Location = new Point(txt_taomatkhau.Width - btn_showHidePassword2.Width, 0);
+//            btn_showHidePassword2.MouseDown += (s, e) => txt_taomatkhau.UseSystemPasswordChar = false;
+  //          btn_showHidePassword2.MouseUp += (s, e) => txt_taomatkhau.UseSystemPasswordChar = true;
+            txt_taomatkhau.Controls.Add(btn_showHidePassword2);
+
+            Button btn_showHidePassword3 = new Button();
+            btn_showHidePassword3.Text = "👁";
+            btn_showHidePassword3.Size = new Size(30, 27);
+            btn_showHidePassword3.Location = new Point(txt_xnmatkhau.Width - btn_showHidePassword3.Width, 0);
+//            btn_showHidePassword3.MouseDown += (s, e) => txt_xnmatkhau.UseSystemPasswordChar = false;
+  //          btn_showHidePassword3.MouseUp += (s, e) => txt_xnmatkhau.UseSystemPasswordChar = true;
+            txt_xnmatkhau.Controls.Add(btn_showHidePassword3);
+
+            Button btn_showHidePassword4 = new Button();
+            btn_showHidePassword4.Text = "👁";
+            btn_showHidePassword4.Size = new Size(30, 27);
+            btn_showHidePassword4.Location = new Point(txt_matkhaumoi.Width - btn_showHidePassword4.Width, 0);
+    //        btn_showHidePassword4.MouseDown += (s, e) => txt_matkhaumoi.UseSystemPasswordChar = false;
+      //      btn_showHidePassword4.MouseUp += (s, e) => txt_matkhaumoi.UseSystemPasswordChar = true;
+            txt_matkhaumoi.Controls.Add(btn_showHidePassword4);
+
+            Button btn_showHidePassword5 = new Button();
+            btn_showHidePassword5.Text = "👁";
+            btn_showHidePassword5.Size = new Size(30, 27);
+            btn_showHidePassword5.Location = new Point(txt_xnmatkhaumoi.Width - btn_showHidePassword5.Width, 0);
+        //    btn_showHidePassword5.MouseDown += (s, e) => txt_xnmatkhaumoi.UseSystemPasswordChar = false;
+          //  btn_showHidePassword5.MouseUp += (s, e) => txt_xnmatkhaumoi.UseSystemPasswordChar = true;
+            txt_xnmatkhaumoi.Controls.Add(btn_showHidePassword5);
         }
 
         #endregion
 
+        private Panel pnl_dangnhap;
+        private TextBox txt_matkhau;
+        private Label lbl_quenmatkhau;
+        private Label lbl_matkhau;
+        private Label lbl_taotaikhoan;
+        private Label lbl_taikhoan;
+        private TextBox txt_taikhoan;
         private Button button1;
+        private Panel pnl_taotaikhoan;
+        private TextBox txt_xnmatkhau;
+        private TextBox textBox5;
+        private TextBox txt_taomatkhau;
+        private TextBox textBox3;
+        private Label label7;
+        private Label label3;
+        private Label label5;
+        private TextBox txt_taotaikhoan;
+        private Label label6;
+        private Button btn_taomatkhau;
+        private Panel pnl_quenmatkhau;
+        private TextBox txt_xnmatkhaumoi;
+        private TextBox textBox2;
+        private TextBox txt_matkhaumoi;
+        private TextBox textBox6;
+        private Label label1;
+        private Label label2;
+        private Label label4;
+        private Button btn_capnhatmatkhau;
+        private TextBox txt_timtaikhoan;
+        private Label label8;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -1,17 +1,103 @@
-﻿namespace QLTV
+﻿using System.Data.SqlClient;
+namespace QLTV
 {
     public partial class Form1 : Form
     {
         public Form1()
         {
             InitializeComponent();
+            this.Text = "Đăng nhập";
         }
 
-       
-
-        private void button1_Click_1(object sender, EventArgs e)
+        private void lbl_taotaikhoan_Click(object sender, EventArgs e)
         {
-            MessageBox.Show(("Nhanh lên!!!") + MessageBox.Show("Làm bài đi"));
+            pnl_dangnhap.Visible = false;
+            pnl_taotaikhoan.Visible = true;
+            pnl_taotaikhoan.Location = pnl_dangnhap.Location;
+            this.Text = "Tạo tài khoản";
+        }
+
+        private void lbl_quenmatkhau_Click(object sender, EventArgs e)
+        {
+            pnl_dangnhap.Visible = false;
+            pnl_quenmatkhau.Visible = true;
+            pnl_quenmatkhau.Location = pnl_dangnhap.Location;
+            this.Text = "Quên mật khẩu";
+        }
+
+        private void txt_taikhoan_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsLetterOrDigit(e.KeyChar) && e.KeyChar != '@' && e.KeyChar != '.' && !char.IsControl(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void txt_matkhau_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (txt_matkhau.Text.Length >= 16 && !char.IsControl(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void txt_taotaikhoan_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsLetterOrDigit(e.KeyChar) && e.KeyChar != '@' && e.KeyChar != '.' && !char.IsControl(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void txt_taomatkhau_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (txt_taomatkhau.Text.Length >= 16 && !char.IsControl(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void txt_xnmatkhau_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (txt_xnmatkhau.Text.Length >= 16 && !char.IsControl(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void btn_taomatkhau_Click(object sender, EventArgs e)
+        {
+            pnl_taotaikhoan.Visible = false;
+            pnl_dangnhap.Visible = true;
+            this.Text = "Đăng nhập";
+        }
+
+        private void txt_timtaikhoan_KeyPress(object sender, KeyPressEventArgs e)
+        {
+
+        }
+
+        private void txt_matkhaumoi_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (txt_matkhaumoi.Text.Length >= 16 && !char.IsControl(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void txt_xnmatkhaumoi_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (txt_xnmatkhaumoi.Text.Length >= 16 && !char.IsControl(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void btn_capnhatmatkhau_Click(object sender, EventArgs e)
+        {
+            pnl_quenmatkhau.Visible = false;
+            pnl_dangnhap.Visible = true;
+            this.Text = "Đăng nhập";
         }
     }
 }
